c58220109eee14dbb14958067e04d0f71c4a85f1
Lauren Mastroni <laurenmastroni@gmail.com>
2019-01-16 18:55:45 +0800

    add new pages

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function pureb2b_script_enqueue() {
-  wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/pureb2b.css', array(), '1.3.5', 'all');
+  wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/pureb2b.css', array(), '1.4.3', 'all');
   wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js', array(), '3.3.1', true);
   wp_enqueue_script('customjs', get_template_directory_uri() . '/js/pureb2b.js', array('jquery'), '1.0.6', true);
 }
